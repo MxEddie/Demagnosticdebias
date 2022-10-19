19a1b33e154acf80ae2e1a5ca106e17fcfceaad6
MxEddie <56070800+MxEddie@users.noreply.github.com>
2022-10-19 12:56:36 +0100

    Update and rename 4debias.sh to debias.sh

--- context_debias/script/debias.sh
+++ context_debias/script/debias.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-#SBATCH -A NLP-CDT-SL2-GPU
-#SBATCH -p ampere
-#SBATCH -N1
-#SBATCH -n1
-#SBATCH --gres=gpu:1
-#SBATCH -t 12:0:0
-
-. /etc/profile.d/modules.sh
-module load rhel8/default-amp
-module load python/3.6
-
-source ../../cd/bin/activate
-
 model_type=bert
 seed=21
 
@@ -27,13 +13,13 @@ if [ $model_type = 'bert' ]; then
     model_name_or_path=bert-base-cased
 fi
 
-TRAIN_DATA=/rds/user/$USER/hpc-work/preprocess/$seed/$model_type/data.bin/data.bin
-OUTPUT_DIR=/rds/user/$USER/hpc-work/debiased_models/$seed/$model_type/$lr/$alpha
+TRAIN_DATA=/preprocess/$seed/$model_type/data.bin/data.bin
+OUTPUT_DIR=/debiased_models/$seed/$model_type/$lr/$alpha
 
 rm -r $OUTPUT_DIR
 echo $model_type $seed
 
-CUDA_VISIBLE_DEVICES=$gpu python -u ../src/4run.py \
+CUDA_VISIBLE_DEVICES=$gpu python -u ../src/run.py \
     --output_dir=$OUTPUT_DIR \
     --model_type=$model_type \
     --model_name_or_path=$model_name_or_path \
