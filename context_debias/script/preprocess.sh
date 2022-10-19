78e99188c3046d499f41b86a76b8736f808357a4
MxEddie <56070800+MxEddie@users.noreply.github.com>
2022-10-19 12:55:01 +0100

    Update and rename new_preprocess.sh to preprocess.sh

--- context_debias/script/preprocess.sh
+++ context_debias/script/preprocess.sh
@@ -1,26 +1,12 @@
-#!/bin/bash
-#SBATCH -A NLP-CDT-SL2-GPU
-#SBATCH -p ampere
-#SBATCH -N1
-#SBATCH -n1
-#SBATCH --gres=gpu:1
-#SBATCH -t 12:0:0
-
-. /etc/profile.d/modules.sh
-module load rhel8/default-amp
-module load python/3.6
-
-source ../../cd/bin/activate
-
 model_type=bert
 seed=$1
 block_size=512
-OUTPUT_DIR=/rds/user/$USER/hpc-work/preprocess/$seed/$model_type/data.bin
+OUTPUT_DIR=/output_dir/
 
 rm -r $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-python3 -u ../src/2preprocess.py --input /rds/user/hpcungl1/hpc-work/reddit_cd/RC_2018 \
+python3 -u ../src/2preprocess.py --input /input_data \
                         --stereotypes ../data/warm_stereotypes.txt,../data/comp_stereotypes.txt \
                         --attributes ../data/EA_CD.txt,../data/AA_MF_CD.txt \
                         --output $OUTPUT_DIR \
